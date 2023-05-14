fd3190bb10e4611a48dc40e6788de6806b83a72c
Gabriel Agrela <100cabessa@gmail.com>
2023-05-14 18:51:07 +0100

    refactor SA

--- sentimentAnalyzer.js
+++ sentimentAnalyzer.js
@@ -1,4 +1,3 @@
-// Configuration
 const config = {
   checkUrlInterval: 500,
   maxCommentLength: 200,
@@ -10,54 +9,34 @@ const config = {
   temperature: 0.8,
 };
 
-class SentimentAnalyzer
-{
-  constructor()
-  {
+class SentimentAnalyzer {
+  constructor() {
     this.CSSManager = new CSSManager();
     this.apiKeyManager = new ApiKeyManager();
-    this.analysisManager = new AnalysisManager(config, this.apiKeyManager); // Update this line
+    this.analysisManager = new AnalysisManager(config, this.apiKeyManager);
     this.buttonManager = new ButtonManager(this.analysisManager, this.CSSManager);
-    this.lastUrl = "";
     this.watchUrlChange();
   }
 
-  watchUrlChange()
-  {
+  watchUrlChange() {
+    this.lastUrl = "";
+
     const isRedditCommentUrl = /^https?:\/\/(www\.)?reddit\.com\/r\/.+?\/comments\/.+?\/.+?\/?$/i;
     const isRedditUrl = /^https?:\/\/(www\.)?reddit\.com(\/(r\/[A-Za-z0-9_]+(\/new)?|hot|best|new))?\/?$/i;
 
-    const checkUrlChange = () =>
-    {
-      if (this.lastUrl !== window.location.href)
-      {
-        let iterator = this.buttonManager.processedThreadIDs.values();
+    const checkUrlChange = () => {
+      if (this.lastUrl !== window.location.href) {
+        this.resetProcessedThreadIDs();
+      }
 
-        for (let value of iterator)
-        {
-          console.log("nwnwn1 " + value);
-        }
-        this.buttonManager.processedThreadIDs.clear();
-        for (let value of iterator)
-        {
-          console.log("nwnwn2 " + value);
-        }
+      if (isRedditCommentUrl.test(window.location.href) && this.lastUrl !== window.location.href) {
+        setTimeout(() => this.mainInThread(), 1);
       }
-      
-      if (isRedditCommentUrl.test(window.location.href) && this.lastUrl !== window.location.href)
-      {
-        setTimeout(() =>
-        {
-          this.mainInThread();
-        }, 1);
+
+      if (isRedditUrl.test(window.location.href)) {
+        setTimeout(() => this.mainInPage(), 1);
       }
-      if (isRedditUrl.test(window.location.href))
-      {
-        setTimeout(() =>
-        {
-          this.mainInPage();
-        }, 1);
-      }
+
       this.lastUrl = window.location.href;
       setTimeout(checkUrlChange, config.checkUrlInterval);
     };
@@ -65,16 +44,19 @@ class SentimentAnalyzer
     checkUrlChange();
   }
 
-  async mainInThread()
-  { 
-    this.buttonManager.addButtonsInThread(await this.apiKeyManager.getStoredApiKey());
+  resetProcessedThreadIDs() {
+    this.buttonManager.processedThreadIDs.clear();
   }
 
-  async mainInPage()
-  {
-
-    this.buttonManager.addButtonsInPage(await this.apiKeyManager.getStoredApiKey());
+  async mainInThread() {
+    const apiKey = await this.apiKeyManager.getStoredApiKey();
+    this.buttonManager.addButtonsInThread(apiKey);
   }
 
+  async mainInPage() {
+    const apiKey = await this.apiKeyManager.getStoredApiKey();
+    this.buttonManager.addButtonsInPage(apiKey);
+  }
 }
+
 window.SentimentAnalyzer = SentimentAnalyzer;
